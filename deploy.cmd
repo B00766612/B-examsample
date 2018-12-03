470fe887c143f417154b0a0d39628b81153836ff
Gillian <Macnicholas-G@ulsteruniversity.com>
2018-12-03 22:06:47 +0000

    fix deploy

--- deploy.cmd
+++ deploy.cmd
@@ -6,7 +6,8 @@
 :: Version: 1.
 :: ----------------------
 
-
-copy D:\home\site\repository\B00766612examsample\target %DEPLOYMENT_TARGET%\webapps\
+cd D:\home\site\repository\B00766612examsample\target\
+::copy D:\home\site\repository\B00766612examsample\target\*.war %DEPLOYMENT_TARGET%\webapps\*.war
 rename *.war ROOT.war
+copy *.war %DEPLOYMENT_TARGET%\webapps\*.war
 :: SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot\webapps
