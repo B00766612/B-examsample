bf82f2edb6c030c7d575c4bb7e8c40a8356c7913
Gillian <Macnicholas-G@ulsteruniversity.com>
2018-12-03 22:15:18 +0000

    fixdeploy3

--- deploy.cmd
+++ deploy.cmd
@@ -7,7 +7,8 @@
 :: ----------------------
 
 ::cd D:\home\site\repository\B00766612examsample\target\
-copy D:\home\site\repository\B00766612examsample\target\*.war %DEPLOYMENT_TARGET%\webapps\*.war
+copy D:\home\site\repository\B00766612examsample\target\*.war D:\home\site\wwwroot\webapps\*.war
+::%DEPLOYMENT_TARGET%\webapps\*.war
 ::rename *.war ROOT.war
 ::copy *.war %DEPLOYMENT_TARGET%\webapps\*.war
 :: SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot\webapps
