da8ebaa2c861142b6cd668044e76450ce1ab6332
Gillian <Macnicholas-G@ulsteruniversity.com>
2018-12-03 23:20:29 +0000

    final6

--- deploy.cmd
+++ deploy.cmd
@@ -7,11 +7,11 @@
 :: ----------------------
 
 del D:\home\site\wwwroot\webapps\*.war
-::cd D:\home\site\repository\B00766612examsample\target\
+cd D:\home\site\repository\B00766612examsample\target\
 ::copy D:\home\site\repository\B00766612examsample\target\*.war D:\home\site\wwwroot\webapps\*.war
 ::copy D:\home\site\repository\B00766612examsample\target\*.war D:\home\site\wwwroot\webapps\*.war
 ::%DEPLOYMENT_TARGET%\webapps\*.war
 ::rename *.war ROOT.war
-copy D:\home\site\repository\B00766612examsample\target\*.war D:\home\site\wwwroot\webapps\*.war
+copy *.war D:\home\site\wwwroot\webapps\*.war
 rename D:\home\site\wwwroot\webapps\*.war D:\home\site\wwwroot\webapps\ROOT.war
 :: SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot\webapps
